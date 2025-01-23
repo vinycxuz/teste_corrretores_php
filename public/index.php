393856b6fc19e92b2e8eb5200a4750c3bd3de30a
vinycxuz <vinyby@gmail.com>
2025-01-23 19:16:51 -0300

    feat: criação do formulario em html

--- public/index.php
+++ public/index.php
@@ -6,6 +6,12 @@
   <title>Document</title>
 </head>
 <body>
-  <p>ola</p>
+  <h2>Cadastro de Corretor</h2>
+  <form action="" method="post">
+    <input type="text" name="cpf" placeholder="Digite seu cpf">
+    <input type="text" name="cresci" placeholder="Digite seu cresci">
+    <input type="text" name="nome" placeholder="Digite seu nome">
+    <button type="submit" value="Submit">Enviar</button>
+  </form>
 </body>
 </html>
