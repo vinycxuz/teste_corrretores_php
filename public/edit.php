0afa740e57e689a4b31f3593118d0e5d8f4c1954
vinycxuz <vinyby@gmail.com>
2025-01-24 00:51:44 -0300

    feat: craido tabela e teste de conexão no arquivo edit.php que modificará os usuários no banco de dados

--- public/edit.php
+++ public/edit.php
@@ -1,13 +1,35 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Editar corretor</title>
 </head>
 <body>
-  <p>teste</p>
+  <?php
+  $host = "localhost";
+  $dbname = "corretores";
+  $username = "root";
+  $password = "1396758zafqkI23.";
+
+  try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão realizada com sucesso";
+  } catch(PDOException $e) {
+    echo "Erro de conexão: " . $e->getMessage();
+  }
+
+  ?>
+
+  <form method="post" action="">
+    <label for="cpf">CPF:</label>
+    <input type="text" id="cpf" name="cpf" value="<?php echo $row['cpf']; ?>"><br>
+    <label for="cresci">CRESCI:</label>
+    <input type="text" id="cresci" name="cresci" value="<?php echo $row['cresci']; ?>"><br>
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome" value="<?php echo $row['nome']; ?>"><br>
+    <input type="submit" value="Update">
+  </form>
 </body>
 </html>
